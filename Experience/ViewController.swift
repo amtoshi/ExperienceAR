3bfc52b7e30d0dfa4a3e83c36c3b7b1f8415b59a
Ashutosh Mane <ashutosh@blabby.io>
2020-01-17 16:03:27 +0530

    Initial Commit

---
+++ Experience/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Experience
+//
+//  Created by Ashutosh Mane on 17/01/20.
+//  Copyright © 2020 Ashutosh Mane. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
